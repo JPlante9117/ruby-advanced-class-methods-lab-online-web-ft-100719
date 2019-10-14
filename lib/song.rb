3bc6580b7573b8ceb3abd69ca24132f6f85c6804
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-14 13:44:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,14 @@ class Song
   end
   
   def self.new_by_name(name)
-    song = self.new
+    #song = self.new
     song.name = name
     song
+  end
+  
+  def self.create_by_name
+    song = self.new
+    
   end
 
   def self.all
